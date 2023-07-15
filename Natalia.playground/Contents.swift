65af3e85afd0e3f04b4c92b56470f0aeeccb6dd0
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-15 10:12:05 -0500

    Sum of even numbers

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,3 +1,10 @@
 import Foundation
 
-print("Natallia's exercises from SENA")
+//Realiza la suma de todos los números pares entre 1 y 1000
+var sum = 0
+for index in 1 ..< 1000{
+    if index % 2 == 0{
+        sum += index
+    }
+}
+print(sum)
