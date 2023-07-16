88b213c61b6d2b51ac9f47ad8257067dda095913
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 15:25:36 -0500

    Calcula el valor de una matricula

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,26 +1,31 @@
 import Foundation
 
-//
-var codigo = 120420
-var cif1 = codigo / 100000
-var mod1 = codigo % 100000
-var cif2 = mod1 / 10000
-var mod2 = mod1 % 10000
-var cif3 = mod2 / 1000
-var mod3 = mod2 % 1000
-var cif4 = mod3 / 100
-var mod4 = mod3 % 100
-var cif5 = mod4 / 10
-var cif6 = mod4 % 10
-
-print(" Hermanos: \(cif1)\n Edad: \(cif2*10+cif3)")
-if cif4 % 2 == 0{
-    print(" Amigos: \(cif4)")
-}else{
-    print(" Amigas: \(cif4)")
+//Calcular el valor de una matricula
+var codigo = 12345
+var cif1 = codigo / 10000
+var mod1 = codigo % 10000
+var cif2 = mod1 / 1000
+var mod2 = mod1 % 1000
+var cif3 = mod2 / 100
+var mod3 = mod2 % 100
+var cif4 = mod3 / 10
+var cif5 = mod3 % 10
+var valorMatricula:Double = 0
+if cif1 == 1 {
+    valorMatricula += 300000
+}else if cif1 == 2{
+    valorMatricula += 150000
+}else if(cif1 == 3){
+    valorMatricula += 100000
 }
-if cif5 + cif6 <= 31{
-    print(" Dia de Cumpleaños: \(cif5 + cif6)")
+if cif2 == 1{
+    valorMatricula += valorMatricula * 0.5
 }else{
-    print("Día de cumpleaños incorrecto")
+    valorMatricula += valorMatricula * 0.10
+}
+
+if ((cif3 * 100) + (cif4 * 10) + cif5) % 2 == 0{
+    valorMatricula -= valorMatricula * 0.20
+}else{
+    valorMatricula -= valorMatricula * 0.10
 }
