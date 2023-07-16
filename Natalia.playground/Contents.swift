3dae8e780f0e502f9766b1e31a71299e743a5efc
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 15:51:35 -0500

    Calcular el valor a pagar en un café internet

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,31 +1,14 @@
 import Foundation
 
-//Calcular el valor de una matricula
-var codigo = 12345
-var cif1 = codigo / 10000
-var mod1 = codigo % 10000
-var cif2 = mod1 / 1000
-var mod2 = mod1 % 1000
-var cif3 = mod2 / 100
-var mod3 = mod2 % 100
-var cif4 = mod3 / 10
-var cif5 = mod3 % 10
-var valorMatricula:Double = 0
-if cif1 == 1 {
-    valorMatricula += 300000
-}else if cif1 == 2{
-    valorMatricula += 150000
-}else if(cif1 == 3){
-    valorMatricula += 100000
-}
-if cif2 == 1{
-    valorMatricula += valorMatricula * 0.5
-}else{
-    valorMatricula += valorMatricula * 0.10
-}
-
-if ((cif3 * 100) + (cif4 * 10) + cif5) % 2 == 0{
-    valorMatricula -= valorMatricula * 0.20
-}else{
-    valorMatricula -= valorMatricula * 0.10
+//Calcular el valor a pagar en un café internet
+var valor = 0
+var minutos = 320
+if minutos <= 15{
+    valor = 500
+}else if minutos >= 16 && minutos <= 30{
+    valor = 1000
+}else if minutos >= 31 && minutos <= 60{
+    valor = 1400
+}else if minutos > 60{
+    valor = 20 * minutos
 }
