638a6e5153eeff8d6c8146e95431f88bd16acaeb
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 09:00:02 -0500

    Solving a formula

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,11 +1,6 @@
 import Foundation
 
-//Dado un numero entero de cuatro cifras invertirlo
-let num = 1234
-var op = num / 1000
-var aux = num % 1000
-var op1 = aux / 100
-var aux1 = aux % 100
-var op2 = aux1 / 10
-var op3 = aux1 % 10
-var number = (op3 * 1000) + (op2 * 100) + (op1 * 10) + op
+//Resolver formula dados los valores de A y B --> ((a + b) ^ 2) / 3
+let a = 3
+let b = 7
+var form = ((a + b) * (a + b)) / 3
