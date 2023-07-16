a91d66c4aecaaf1f16fa791bbc91e01bf7354972
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 16:43:18 -0500

    Calcular el precio a pagar por una llamada en una empresa de minutos

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,14 +1,23 @@
 import Foundation
 
-//Calcular el valor a pagar en un café internet
-var valor = 0
-var minutos = 320
-if minutos <= 15{
-    valor = 500
-}else if minutos >= 16 && minutos <= 30{
-    valor = 1000
-}else if minutos >= 31 && minutos <= 60{
-    valor = 1400
-}else if minutos > 60{
-    valor = 20 * minutos
+//Calcular el precio de la venta de minutos
+var minutos:Double = 120
+var segundos:Double = 60
+var valorPagar:Double = 0
+if segundos > 30 && segundos <= 60{
+    minutos += 1
 }
+if minutos >= 1 && minutos <= 5{
+    valorPagar += 300 * minutos
+    print(valorPagar)
+}
+if minutos > 5 && minutos <= 10{
+    valorPagar += (200 * (minutos - 5)) + 1500
+}
+if minutos > 10{
+    valorPagar = (100 * (minutos - 10)) + 1500
+}
+if valorPagar > 1600{
+    valorPagar -= valorPagar * 0.10
+}
+print(valorPagar)
