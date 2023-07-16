2b7ff239eedffeb20e45fe054a832b59834068b8
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 09:08:13 -0500

    Calcular el promedio

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,6 +1,12 @@
 import Foundation
 
-//Resolver formula dados los valores de A y B --> ((a + b) ^ 2) / 3
-let a = 3
-let b = 7
-var form = ((a + b) * (a + b)) / 3
+//Dada la matricula y cinco calificaciones de un alumno obtenidas a lo largo del semestre, construye un diagrama de flujo que imprima la matricula del estudiante y el promedio de sus calificaciones
+let matricula = "A20230001"
+let nombre = "Jeisson Chacón"
+var materia = 3.8
+var materia1 = 4.5
+var materia2 = 3.5
+var materia3 = 3.9
+var materia4 = 4.2
+var promedio = (materia + materia1 + materia2 + materia3 + materia4) / 5
+print(" Nombre: \(nombre)\n Matricula: \(matricula)\n Promedio: \(promedio)")
