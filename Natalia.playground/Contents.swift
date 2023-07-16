0a6a1c5cee9be8566964fa9c99ab78cd71a00c20
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 08:53:56 -0500

    Reverse a number given

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,8 +1,11 @@
 import Foundation
 
-//Leer el radio de un circulo, calcular la superficie y longitud de la circunferencia.
-var r:Double = 3
-let pi = 3.1416
-var area:Double = pi * (r * r)
-var longitud:Double = 2 * pi * r
-print(" Radio: \(r) \n Area: \(area) \n Longitud: \(longitud)")
+//Dado un numero entero de cuatro cifras invertirlo
+let num = 1234
+var op = num / 1000
+var aux = num % 1000
+var op1 = aux / 100
+var aux1 = aux % 100
+var op2 = aux1 / 10
+var op3 = aux1 % 10
+var number = (op3 * 1000) + (op2 * 100) + (op1 * 10) + op
