90fbebebb30cb8dc8fca7ed9b1c8acb9472d40d8
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-16 13:14:46 -0500

    Descomponer código para sacar la edad, numero de amigos y día de nacimiento

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,12 +1,26 @@
 import Foundation
 
-//Dada la matricula y cinco calificaciones de un alumno obtenidas a lo largo del semestre, construye un diagrama de flujo que imprima la matricula del estudiante y el promedio de sus calificaciones
-let matricula = "A20230001"
-let nombre = "Jeisson Chacón"
-var materia = 3.8
-var materia1 = 4.5
-var materia2 = 3.5
-var materia3 = 3.9
-var materia4 = 4.2
-var promedio = (materia + materia1 + materia2 + materia3 + materia4) / 5
-print(" Nombre: \(nombre)\n Matricula: \(matricula)\n Promedio: \(promedio)")
+//
+var codigo = 120420
+var cif1 = codigo / 100000
+var mod1 = codigo % 100000
+var cif2 = mod1 / 10000
+var mod2 = mod1 % 10000
+var cif3 = mod2 / 1000
+var mod3 = mod2 % 1000
+var cif4 = mod3 / 100
+var mod4 = mod3 % 100
+var cif5 = mod4 / 10
+var cif6 = mod4 % 10
+
+print(" Hermanos: \(cif1)\n Edad: \(cif2*10+cif3)")
+if cif4 % 2 == 0{
+    print(" Amigos: \(cif4)")
+}else{
+    print(" Amigas: \(cif4)")
+}
+if cif5 + cif6 <= 31{
+    print(" Dia de Cumpleaños: \(cif5 + cif6)")
+}else{
+    print("Día de cumpleaños incorrecto")
+}
