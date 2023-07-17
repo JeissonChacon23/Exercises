b503427e87c30163d0629109909a9e19a9fe1bc3
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-17 06:59:17 -0500

    Validar el código de una boleta para la entrada a un evento

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,27 +1,33 @@
 import Foundation
 
-//Se requiere calcular el valor neto a pagar a un trabajador (Contratistas y de Planta) en un mes, sabiendo que cada trabajadortiene un sueldo básico y teniendo en cuenta lo siguiente: A todo trabajador de planta, se le debe descontar el 4% del sueldo básico por concepto de salud y el 3.8% del sueldo básico por concepto de pensión. Si el trabajador es de Planta y su sueldo básico es de menos de $ 437.000 se le debe cancelar $ 40.000 adicional por concepto de auxilio de transporte. Si el trabajador es de Planta y devenga mas de $ 1.750.000 de salario básico, se le descontara el 1% del sueldo básico por concepto de Solidaridad Pensional. Si el trabajador es Contratista, se le debe descontar el 10% por concepto de retención en la fuente solamente si su salario es superior a $ 1.500.000.
+//En la ciudad de Pamplona se va a realizar un concierto por el dia de amor y amistad todas las boletas están identificadas con un numero de 6 cifras, que esta compuesto de la siguiente forma: Si la suma del primer y el último digito es mayor que el digito cinco la boleta es valida, en caso contrario no es valida.El segundo Digito determina el tipo de Boleta según la siguiente tablaTipo de Entrada De 1 a 5 GramillaDe 6 a 9 incluyendo 0 GeneralEl numero que forma el tercer y cuarto digito determina la hora y la puerta de entrada al evento según la siguiente tabla Si es par Puerta 1 a partir de las 7 pm Si es impar Puerta 2 a partir de las 8 pm Analice y diseñe un algoritmo donde se ingrese el numero de una boleta y posteriormente determine: Si la boleta es valida, el tipo de boleta y la puerta y horario al cual debe ingresar el espectador.
 
-var valorNeto = 0.0
-var sueldoBasico = 1780000.0
-var tipoTrabajador = 1 // 1.) Contratista 2.) Planta
-print("Sueldo Basico: \(sueldoBasico)")
+var codigo = 437952
+var cif1 = codigo / 100000
+var mod1 = codigo % 100000
+var cif2 = mod1 / 10000
+var mod2 = mod1 % 10000
+var cif3 = mod2 / 1000
+var mod3 = mod2 % 1000
+var cif4 = mod3 / 100
+var mod4 = mod3 % 100
+var cif5 = mod4 / 10
+var cif6 = mod4 % 10
 
-if tipoTrabajador == 2 && sueldoBasico >= 437000{
-    sueldoBasico -= (sueldoBasico * 7.8) / 100
-}else if tipoTrabajador == 2 && sueldoBasico < 437000{
-    sueldoBasico += 40000
-}else if tipoTrabajador == 2 && sueldoBasico < 1750000{
-    sueldoBasico -= sueldoBasico * 0.1
-}
-
-if tipoTrabajador == 1 && sueldoBasico > 1500000{
-    sueldoBasico -= sueldoBasico * 0.10
-}
-
-if tipoTrabajador == 1{
-    print("Tipo de Trabajador: Contratista")
+if cif1 + cif6 > cif5{
+    print("Voleta Valida")
+    if cif2 >= 1 && cif2 <= 5{
+        print("Tipo de Entrada: Gramilla")
+    }else{
+        print("Tipo de Entrada: General")
+    }
+    
+    if ((cif3 * 100) + cif4) % 2 == 0{
+        print("Puerta: 1 \nHora: 19:00")
+    }else{
+        print("Puerta: 2 \nHora: 20:00")
+    }
+    
 }else{
-    print("Tipo de Trabajador: Planta")
+    print("Voleta Invalida")
 }
-print("Sueldo Final: \(sueldoBasico)")
