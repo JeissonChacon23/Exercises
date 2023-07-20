a24f6523753c99442fa69473064d8f8ea7401608
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-19 19:34:40 -0500

    Calcular el salario final a pagar a un empleado

--- Natalia.playground/Contents.swift
+++ Natalia.playground/Contents.swift
@@ -1,47 +1,21 @@
 import Foundation
 
-//Calcular el valor a pagar de la factura del celular conociendo el tipo de plan, los minutos y la cantidad de mensajes de texto consumidos en el mes, segun la siguiente tabla: Tipo de plan Minutos Básicos Mensajes T Básicos Cargo Básico 1. Ideal 100 10 80.000 2. Extensión 135 20 120.000 3. Familiar 425 30 180.000 Cada minuto extra tiene un costo de 855 pesos y los mensajes de texto un costo de 370 pesos (Para cualquier plan)
-var valor = 0
-var minuto = 0
-var mensaje = 0
-var planIdeal = [100, 10, 80000]
-var planExtension = [135, 20, 120000]
-var planFamiliar = [425, 30, 180000]
-var tipoPlan = planFamiliar
-var consumo = [426, 20, 80000]
+//Calcular el salario final a pagar a un empleado a quien se le paga a X pesos la hora trabajada. Se hace un descuento de acuerdoa la siguiente tabla comparativa con el salario minimo (dado por teclado) si el salario esta entre Descuento 1 y 2 (incluido) salarios minimos 5% 2 y 6 (incluido) salarios minimos 7% Mas de 6 salarios minimos 10% En caso de que el salario sea menor a un salario minimo se le dara una bonificacion de 20.000 pesos.
 
-if tipoPlan.elementsEqual(planIdeal) && (consumo[0] > 100 || consumo[1] > 10){
-    valor += planIdeal[2]
-    if consumo[0] > 100{
-        valor += ((consumo[0] - planIdeal[0]) * 855)
-    }
-    if consumo[1] > 10{
-        valor += ((consumo[1] - planIdeal[1]) * 370)
-    }
-}else if tipoPlan.elementsEqual(planIdeal) && consumo[0] <= 100 && consumo[1] <= 10{
-    valor += planIdeal[2]
-}
-if tipoPlan.elementsEqual(planExtension) && (consumo[0] > 135 || consumo[1] > 20){
-    valor += planExtension[2]
-    if consumo[0] > 135{
-        valor += ((consumo[0] - planExtension[0]) * 855)
-    }
-    if consumo[1] > 20{
-        valor += ((consumo[1] - planExtension[1]) * 370)
-    }
-}else if tipoPlan.elementsEqual(planExtension) && consumo[0] <= 135 && consumo[1] <= 20{
-    valor += planExtension[2]
-}
-if tipoPlan.elementsEqual(planFamiliar) && (consumo[0] > 425 || consumo[1] > 30){
-    valor += planFamiliar[2]
-    if consumo[0] > 425{
-        valor += ((consumo[0] - planFamiliar[0]) * 855)
-    }
-    if consumo[1] > 30{
-        valor += ((consumo[1] - planFamiliar[1]) * 370)
-    }
-}else if tipoPlan.elementsEqual(planFamiliar) && consumo[0] <= 425 && consumo[1] <= 30{
-    valor += planFamiliar[2]
-}
-valor
+var salarioMinimo:Double = 1300000
+var hora:Double = 192
+var precioHora:Double = 15000
+var salario:Double = hora * precioHora
 
+if salario >= salarioMinimo && salario <= salarioMinimo * 2{
+    salario -= salario * 0.05
+}
+if salario > salarioMinimo * 2 && salario <= salarioMinimo * 6{
+    salario -= salario * 0.07
+}
+if salario > salarioMinimo * 6{
+    salario -= salario * 0.10
+}
+if salario < salarioMinimo{
+    salario += 20000
+}
